defeba80956648c1e26899fa6ca5fb9e260a0284
Tomislav Simovic <tomislav.simovic@ericsson.com>
2023-07-29 19:47:26 +0200

    Initial commit

---
+++ src/main/java/hr/tsimovic/expansetracker/ExpanseTrackerApplication.java
@@ -0,0 +1,13 @@
+package hr.tsimovic.expansetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpanseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpanseTrackerApplication.class, args);
+	}
+
+}
